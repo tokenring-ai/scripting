f4adea71c30b3e79fcb241a7aeab538bc675a144
Mark Dierolf <mdierolf@cloudquote.io>
2026-01-04 06:38:40 -0800

    Make `attach` method in `TokenRingService` synchronous by removing `async`.

--- ScriptingService.ts
+++ ScriptingService.ts
@@ -64,7 +64,7 @@ export default class ScriptingService implements TokenRingService {
     }
   }
 
-  async attach(agent: Agent): Promise<void> {
+  attach(agent: Agent): void {
     agent.initializeState(ScriptingContext, {});
   }
 
