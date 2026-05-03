80f40a4fed634e0bb804e63c49b65f4a200cc3b8
Mark Dierolf <mdierolf@financialcontent.com>
2026-05-02 21:33:40 -0700

    Add `ScriptionFunctionShema` and `ScriptionFunction` types to scripting schema

--- schema.ts
+++ schema.ts
@@ -1,7 +1,17 @@
 import z from "zod";
 
+export const ScriptionFunctionShema = z.object({
+  type: z.enum(["expression", "llm", "js"]),
+  params: z.array(z.string()),
+  body: z.string(),
+});
+
+export type ScriptionFunction = z.infer<typeof ScriptionFunctionShema>;
+
 export const ScriptSchema = z.union([z.string(), z.array(z.string())]);
 export type Script = z.infer<typeof ScriptSchema>;
 
 export const ScriptingServiceConfigSchema = z.record(z.string(), ScriptSchema);
 export type ParsedScriptingServiceConfig = z.output<typeof ScriptingServiceConfigSchema>;
+
+
